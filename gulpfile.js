b80545028892704c9fa9af99df84c3c94401953c
“namakurajin” <namakurajin@gmail.com>
2025-01-18 10:15:34 +0900

    feat: 出力先の掃除

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@ import { src, dest } from 'gulp';
 import imagemin, {mozjpeg, svgo} from 'gulp-imagemin';
 import pngquant from 'imagemin-pngquant';
 import through2 from 'through2';
+import { deleteSync } from 'del';
 
 // コンソールの色付け
 var green = '\u001b[32m';
 var reset = '\u001b[0m';
 
 export default () => (
+  // まず、出力先を空にする
+  deleteSync(['dist/**']),
+
   src('src/**/*.{jpg,png,svg}', { encoding: false })
     // 画像を圧縮
     .pipe(
